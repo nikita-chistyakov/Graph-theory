243520096d16536b3856fb1250f5b405d53929f1
Nikita Chistyakov <51343531+nikita-chistyakov@users.noreply.github.com>
2025-05-20 21:51:17 +0200

    Update app.py

--- app.py
+++ app.py
@@ -115,7 +115,7 @@ with col2:
 ##animation_speed = st.slider("Animation Speed:", min_value=0.5, max_value=3.0, value=1.0, step=0.1)
 ##delay = 1.0 / animation_speed  # Convert to delay
 
-animation_speed = st.slider("Animation Speed:", min_value=1, max_value=5, value=1, step=1)
+animation_speed = st.slider("Animation Speed:", min_value=1, max_value=10, value=1, step=1)
 delay = 0.005 / animation_speed
 
 
